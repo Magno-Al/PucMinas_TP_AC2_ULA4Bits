682118bddbeb67630c03ce61cf33f287027b3793
Magno-Al <am.magno2@gmail.com>
2023-09-27 14:43:22 -0300

    Correcao da comunicacao serial

--- ULA_4Bits/Form1.cs
+++ ULA_4Bits/Form1.cs
@@ -6,38 +6,48 @@ namespace ULA_4Bits
     public partial class Form1 : Form
     {
         SerialCOM SerialCOM = new SerialCOM();
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        #region ComboBox de comunicação serial  
+        #region ComboBox serial communication
         private void comboBox_SerialPorts_DropDown(object sender, EventArgs e)
         {
             FillComboBox();
         }
 
-        private void comboBox_SerialPorts_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            SerialCOM.Port = comboBox_SerialPorts.SelectedItem.ToString();
-
-            ShowCommunicationStatus(SerialCOM.OpenCommunication());
-        }
-
         private void FillComboBox()
         {
             comboBox_SerialPorts.Items.Clear();
+            comboBox_SerialPorts.Items.Add("Nenhum dispositivo");
 
-            comboBox_SerialPorts.Items.Add("Sem conecção");
             foreach (string port in SerialCOM.AutodetectArduinoPort())
             {
                 comboBox_SerialPorts.Items.Add(port);
             }
         }
+
+        private void comboBox_SerialPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SerialCOM.Port = comboBox_SerialPorts.SelectedItem.ToString();
+            if (SerialCOM.OpenCommunication())
+            {
+                lbl_ConnectionStatus.Text = "Conectado";
+                lbl_ConnectionStatus.ForeColor = Color.Green;
+                btn_SendDataToSerial.Enabled = true;
+            }
+            else
+            {
+                lbl_ConnectionStatus.Text = "Não conectado";
+                lbl_ConnectionStatus.ForeColor = Color.Red;
+                btn_SendDataToSerial.Enabled = false;
+                FillComboBox();
+            }
+        }
         #endregion
 
-        #region Botões de tratamento de arquivos
+        #region Convert File Events
         private void btn_openFile_Click(object sender, EventArgs e)
         {
             Stream stream;
@@ -50,8 +60,12 @@ namespace ULA_4Bits
                     string fileName = openFileDialog.FileName;
                     string fileText = File.ReadAllText(fileName);
 
+
+
                     textBoxFileName.Text = fileName;
                     rtb_FileInstructions.Text = fileText;
+
+
 
                     if (fileText.Length > 0)
                         btn_ConvertToHex.Enabled = true;
@@ -63,12 +77,18 @@ namespace ULA_4Bits
         {
             HexConverter hexConverter = new HexConverter();
 
+
+
             string[] fileTextInHex = hexConverter.ConvetToHex(rtb_FileInstructions.Text);
+
+
 
             foreach (string file in fileTextInHex)
             {
                 rtb_HexInstructions.Text += file + "\n";
             }
+
+
 
             rtb_HexInstructions.Enabled = true;
             btn_DownloadHexFile.Enabled = true;
@@ -84,6 +104,8 @@ namespace ULA_4Bits
                     writer.Write(rtb_HexInstructions.Text);
                     writer.Close();
 
+
+
                     MessageBox.Show("Download realizado com sucesso!");
                 }
             }
@@ -93,7 +115,7 @@ namespace ULA_4Bits
             }
         }
 
-        private void btn_SendDataToSerial_Click(object sender, EventArgs e)
+        private async void btn_SendDataToSerial_Click(object sender, EventArgs e)
         {
             bool testConnection = SerialCOM.TestCommunication();
 
@@ -102,26 +124,15 @@ namespace ULA_4Bits
                 if (rtb_HexInstructions.Text.Length > 0)
                 {
                     SerialCOM.Write(rtb_HexInstructions.Text);
+
+                    btn_SendDataToSerial.Enabled = false;
+
+                    await ReadSerialDataAsync();
+
+                    btn_SendDataToSerial.Enabled = true;
                 }
                 else
                     MessageBox.Show("Nada para enviar");
-            }
-            else
-            {
-                ShowCommunicationStatus(false);
-            }
-        }
-        #endregion
-
-        #region Status de comunicação serial 
-        private async void ShowCommunicationStatus(bool status)
-        {
-            if (status)
-            {
-                lbl_ConnectionStatus.Text = "Conectado";
-                lbl_ConnectionStatus.ForeColor = Color.Green;
-                btn_SendDataToSerial.Enabled = true;
-                await ReadSerialDataAsync();
             }
             else
             {
@@ -133,25 +144,24 @@ namespace ULA_4Bits
         }
         #endregion
 
-        #region Async Leitura da comunicação serial 
+        #region Serial Communication Methods
+        static void ExibeLeituraSerial(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Console.WriteLine("Data Received:");
+            Console.Write(indata);
+        }
+
         private async Task ReadSerialDataAsync()
         {
-            string data;
-            bool exit = false;
-            while (!exit)
+            while (true)
             {
-                if (SerialCOM.TestCommunication())
-                {
-                    data = SerialCOM.Read();
+                string data = SerialCOM.Read(); 
 
-                    UpdateSerialMonitor(data);
-                }
-                else
-                {
-                    exit = true;
-                }
+                UpdateSerialMonitor(data);
 
-                await Task.Delay(1000);
+                await Task.Delay(100);
             }
         }
 
@@ -164,6 +174,7 @@ namespace ULA_4Bits
             }
             else
             {
+                // Atualize o rtb_SerialMonitor com os dados lidos
                 rtb_SerialMonitor.AppendText("\n");
                 rtb_SerialMonitor.AppendText(data);
                 rtb_SerialMonitor.ScrollToCaret();
